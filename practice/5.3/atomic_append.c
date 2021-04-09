adb9da3ff03d8406adf34e895574b18bed42096f
cuihua <1533390849@qq.com>
2021-04-09 22:20:15 +0800

    finish 5.3

--- practice/5.3/atomic_append.c
+++ practice/5.3/atomic_append.c
@@ -4,17 +4,49 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
 
 int main(char argc, char *argv[])
 {
     int fd;
+    int flags;
+    ssize_t count;
+    char uselseek;
 
     if (argc < 2) {
         fprintf(stderr, "%s filename num-bytes [x] \n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    
+    count = atoll(argv[2]);
 
+    uselseek = (argc > 3);
+
+    flags = uselseek ? 0 : O_APPEND;
+
+    fd = open(argv[1], O_RDWR | O_CREAT | flags, S_IRUSR | S_IWUSR);
+    if (fd == -1 ) {
+        printf("%s not exist!\n", argv[1]);  
+        exit(EXIT_FAILURE);
+    }
+
+
+    for (count; count > 0; count--) {
+        if (uselseek) {
+            if (lseek(fd, 0, SEEK_END) == -1) {
+                fprintf(stderr, "lseek failed!\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        if (write(fd, "x", 1) != 1) {
+            fprintf(stderr, "write failed!\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    close(fd);
+
+    printf("%ld done\n", (long)getpid());
     exit(EXIT_SUCCESS);
 }
